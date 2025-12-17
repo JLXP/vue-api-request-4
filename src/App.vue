1e757c6a2eb8f7965199cbc8a61d41fe65996ab7
JavierXool <javier.xool@outlook.com>
2025-12-16 23:58:30 -0600

    get request on json-server

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,47 @@
 <template>
   <div>RequestOPedia</div>
-  <hr />
-  <div v-for="user in userObj.users" :key="user.id">
-    <h2>{{ user.name }}</h2>
-    <p>{{ user.email }}</p>
+  <div class="container p-4">
+    <div >
+      <h1 class="text-success text-center">TravelOpedia</h1>
+    </div>
     <hr />
+    <table class="table table-striped table-light">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Days</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="destination in destinationObj.destinationList" :key="destination.id">
+          <td>{{ destination.name }}</td>
+          <td>{{ destination.days }}</td>
+          <td>{{ destination.price }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
+
 </template>
 <script setup>
 import axios from "axios";
 import { onMounted, reactive } from "vue";
 
-const userObj = reactive({
-  users: [],
+const destinationObj = reactive({
+  destinationList: [],
 });
 onMounted(() => {
+  // fetch("https://jsonplaceholder.typicode.com/users")
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //     userObj.users = data;
+  //   });
 
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      userObj.users = data;
-    });
-
-  // axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
-  //   console.log(response.data);
-  //   userObj.users = response.data;
-  // });
+  axios.get("http://localhost:3000/destination").then((response) => {
+    console.log(response.data);
+    destinationObj.destinationList = response.data;
+  });
 });
 </script>
